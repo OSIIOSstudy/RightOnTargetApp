bce4fdbdba59c700f5cb9c6430ffdd6710014687
OSIIOSdev <OSIIOS.dev@gmail.com>
2024-03-15 07:47:45 -0700

    1. Business logic configured

---
+++ RightOnTargetApp/ViewControllers/GuessingValueViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  RightOnTargetApp
+//
+//  Created by OSIIOSdev on 15.03.2024.
+//
+
+import UIKit
+
+final class GuessingValueViewController: UIViewController {
+    // MARK: - @IBOutlets
+    @IBOutlet var valueSlider: UISlider!
+    @IBOutlet var valueLabel: UILabel!
+    
+    // MARK: - LifeCycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    // MARK: - @IBAction Methods
+    @IBAction func verificationButtonPressed() {
+        
+    }
+    
+
+}
+
